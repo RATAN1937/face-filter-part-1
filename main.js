d6de111c6f901566ba4a30c2b5b2ebe5f616ecd2
RATAN1937 <100527038+RATAN1937@users.noreply.github.com>
2022-10-27 14:29:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+function preload()
+{
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() 
+{
+}
+
+function onclick(){
+    save('myFilterImage.png');
+}
+
